674d9baa018bbac1ce255b93a836bdf0a0a42ea6
Imad Ottallah <imadotta@gmail.com>
2022-05-22 22:01:15 -0500

    added butons and communication with api

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,33 @@ import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 import jokeData from '../api/jokeData';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-  `;
-
-  jokeData().then(console.warn);
+const renderToDom = (divId, textToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToRender;
 };
 
-init();
+const htmlBody = () => {
+  let domString = '';
+  domString = '<div id="joke-btn-container"></div><div id="joke-setup"></div><div id="punchLine"></div>';
+  renderToDom('#app', domString);
+};
+
+const init = () => {
+  document.querySelector('#joke-btn-container').innerHTML = `<button style="display:block" id="get-joke-btn" class="btn btn-lg">Get a Joke</button><button style="display:block" id="punchline-btn" class="btn btn-lg">Get Punchline</button>
+  `;
+  document.querySelector('#get-joke-btn').addEventListener('click', () => {
+    jokeData().then((response) => {
+      document.querySelector('#joke-setup').innerHTML = response.setup;
+      document.querySelector('#punchline-btn').addEventListener('click', () => {
+        document.querySelector('#punchLine').innerHTML = response.delivery;
+      });
+    });
+    document.querySelector('#get-joke-btn').style.display = 'none';
+  });
+};
+
+const startApp = () => {
+  htmlBody();
+  init();
+};
+startApp();
